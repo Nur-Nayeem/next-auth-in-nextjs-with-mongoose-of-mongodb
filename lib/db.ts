79ea5cffab47c2d76b6d07801559aa8edf1fb33b
nur-nayeem <nurnayem768@gmail.com>
2025-11-02 10:04:34 +0600

    added mongodb connection configarations

---
+++ lib/db.ts
@@ -0,0 +1,35 @@
+import { connect } from "mongoose";
+
+const mongodbUrl = process.env.MONGODB_URI;
+
+if (!mongodbUrl) {
+  throw new Error("mongodb url is not found");
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDb = async () => {
+  if (cached.conn) {
+    console.log("db connected from cached");
+
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = connect(mongodbUrl).then((c) => c.connection);
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("db connected with promies");
+  } catch (error) {
+    throw error;
+  }
+  return cached.conn;
+};
+
+export default connectDb;
---
+++ model/user.model.ts
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+interface userI {
+  _id?: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
+  image?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<userI>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+// //in nodejs/express we have make model like this:
+// const User = mongoose.model('User',userSchema);
+// export default User;
+// but this method is not good for next js. if we write like this nextjs store that model in an object . when we call this model again. next js tell us this model already exist dont build this model again and give us error.
+
+//So we should make like this:
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
