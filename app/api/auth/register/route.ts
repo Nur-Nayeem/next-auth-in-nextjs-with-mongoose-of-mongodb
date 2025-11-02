222c7acc3de7e6f946d8156fa67865a163a63b24
nur-nayeem <nurnayem768@gmail.com>
2025-11-02 10:05:44 +0600

    create user creation api and next auth authentcation functionality

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,41 @@
+import connectDb from "@/lib/db";
+import User from "@/model/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    // get data from fron end
+    const { name, email, password } = await req.json();
+    await connectDb();
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      return NextResponse.json(
+        {
+          message: "user alrady exist",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    if (password.length < 6) {
+      return NextResponse.json(
+        { message: "password should be at least 6 characte" },
+        { status: 400 }
+      );
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Something went wrong! ${error}` },
+      { status: 500 }
+    );
+  }
+}
